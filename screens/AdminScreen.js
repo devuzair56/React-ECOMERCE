0dbce9b83436c146b26b6343138f4b15b5b6d67d
devuzair56 <72253079+devuzair56@users.noreply.github.com>
2020-10-02 15:14:28 +0500

    Add files via upload

---
+++ screens/AdminScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Orders from "../components/Order";
+
+export default class AdminScreen extends Component {
+  render() {
+    return (
+      <div>
+        <Orders />
+      </div>
+    );
+  }
+}
